dcf19b847eaea30b3e91df8c06ae83188d5de6c5
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-01 19:39:54 -0400

    foot

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -54,9 +54,9 @@ const Footer = () => {
                 </div>
               </li>
               <li className="flex items-start">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4 mt-0.5 text-white/40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-                  <rect x="2" y="4" width="20" height="16" rx="2"></rect>
-                  <path d="m22 7-10 5L2 7"></path>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-4 mt-0.5 text-white/40 flex-shrink-0" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                  <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
                 <span>{contactInfo.agent.email}</span>
               </li>
